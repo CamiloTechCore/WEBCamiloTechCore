acfbb6c65bbfb40658b6fc009c96b66dd3a2826f
CamiloTechCore <brayancamilomolinadev@gmail.com>
2025-11-30 11:07:19 -0500

    Se creo un nuevo proyecto

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@
 //import { useTranslation } from 'react-i18next';
 // Importa tus imágenes de proyecto
 import project1Img from '../assets/project1.png';
+import project2Img from '../assets/Project2.png';
 //const { t } = useTranslation();
 export const projects = [
   {
@@ -14,7 +15,18 @@ export const projects = [
       github: 'https://github.com/CamiloTechCore/CamiloTechCore',
       live: 'https://camilotechcore.vercel.app/',
     },
-  },
+  }, // <--- AQUÍ: Se añadió la llave de cierre y la coma
+  {
+    id: 2,
+    title: 'AVA Lets - Plataforma para gestion y control de tareas',
+    description: 'Esta herramienta permite la creacion y seguiemiento de tareas operativas, pensada para lideres de equipo y coordinadores de cuenta, AVA Lets busca optimizar los tiempos de gestion de actividades e implementar estrategias de control y gestion del tiempo de manera individual.',
+    imageUrl: project2Img,
+    tags: ['AppScripts', 'CSS', 'Sheets', 'Javascript', 'HTML'],
+    links: {
+      github: 'https://github.com/CamiloTechCore/AVA-Lents.git'
+    },
+  }
+];
   // Añade más proyectos aquí...
   // {
   //   id: 2,
@@ -24,5 +36,5 @@ export const projects = [
   //   tags: ['Tableau', 'SQL', 'Data Viz', 'React'],
   //   links: { ... }
   // }
-];
+
 
