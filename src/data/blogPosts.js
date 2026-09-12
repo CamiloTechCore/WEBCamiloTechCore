b756a8757055b603d45b73ab06b526f051102fd8
Camilo Molina <camilo@example.com>
2026-09-12 10:21:00 -0500

    Actualiza blogPosts, añade imagen y elimina vite.svg

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -2,6 +2,7 @@
 
 // 1. Importa la imagen desde su ruta relativa
 import cognitiveScaffoldingImg from '../assets/cognitivescaffolding.png';
+import iaHerramientaEducativaImg from '../assets/Artificial Analysis Intelligence Index v4.3.png';
 
 export const posts = [
   {
@@ -10,8 +11,15 @@ export const posts = [
     title: '5 pasos para el cognitive scaffolding',
     date: '02 de Agosto, 2025',
     summary: 'Este enfoque iterativo garantiza que el proceso de cognitive scaffolding sea dinámico y adaptativo, permitiendo que el aprendizaje sea un ciclo continuo de apoyo, práctica, retroalimentación y autonomía progresiva.',
-    // 2. Usa la variable importada aquí. También corregí el nombre del archivo.
     imageUrl: cognitiveScaffoldingImg,
+  },
+   {
+    id: 2,
+    slug: 'la-ia-como-herramienta-educativa',
+    title: 'IA como erramienta educativa',
+    date: '12 de Septiembre, 2026',
+    summary: '¿Qué tanto la IA ha impactado mi modelo educativo? y ¿como mido por medio de sistemas de performace y curva laboral lo que realmente aprendo usando IA como asistente personal de estudio?',
+    imageUrl: iaHerramientaEducativaImg,
   },
 ];
 
