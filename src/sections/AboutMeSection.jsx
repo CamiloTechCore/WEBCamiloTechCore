79417f425436ba6a7e8cb984819ccd56c3e94275
Camilo Molina <camilo@example.com>
2026-09-12 10:34:32 -0500

    New Service

--- src/sections/AboutMeSection.jsx
+++ src/sections/AboutMeSection.jsx
@@ -91,10 +91,9 @@ function AboutMeSection() {
           <p className="text-gray-600 dark:text-gray-300 leading-relaxed">{t('parragraps.tittle7')}</p>
           <br></br>
           <br></br>
-          <p className="text-gray-600 dark:text-gray-300 leading-relaxed"><strong>{t('hero.subtitle11')} </strong> {t('hero.subtitle11')}</p>
+          <p className="text-gray-600 dark:text-gray-300 leading-relaxed"><strong>{t('hero.subtitle12')} </strong> {t('hero.subtitle12')}</p>
           <br></br>
           <p className="text-gray-600 dark:text-gray-300 leading-relaxed">{t('parragraps.tittle8')}</p>
-          
           </div>
         </div>
       </div>
